5ddb0b936ffddcb6b261d1ea07b6a121e2554d74
Mosawir Shah <92377115+MosawirShah@users.noreply.github.com>
2024-04-19 16:47:18 +0500

    Modularization We have modularized the code,by using different classes

---
+++ lib/model/question.dart
@@ -0,0 +1,11 @@
+class Question{
+  String questionText;
+  bool answerText;
+
+  Question(this.questionText,  this.answerText);
+
+// Question(String questionText, bool answerText){
+//   this.questionText = questionText;
+//   this.answerText = answerText;
+// }
+}
